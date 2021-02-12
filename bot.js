c4b3e50efa9900d992815a108b0aee2bd86d540c
ajkimpso <75821602+ajkimpso@users.noreply.github.com>
2021-02-12 17:14:50 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     //postMessage("http://www.daddyleagues.com/maddenrating?name=&position=all&team="+request.text.substring(5,8));
-    postMessage("http://daddyleagues.com/sfl21/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("https://gamerhuddle.com/league/solid-football-league/schedule"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   
